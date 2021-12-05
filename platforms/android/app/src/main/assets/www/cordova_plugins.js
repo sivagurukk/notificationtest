d7810758d9097030ad901b2eea1196ea6df3d422
unknown <sivagurukk@gmail.com>
2021-12-05 17:15:42 +0530

    tr

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-firebasex.FirebasePlugin",
+      "file": "plugins/cordova-plugin-firebasex/www/firebase.js",
+      "pluginId": "cordova-plugin-firebasex",
+      "clobbers": [
+        "FirebasePlugin"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-firebasex": "13.0.1",
+    "cordova-plugin-whitelist": "1.3.5"
+  };
+});
